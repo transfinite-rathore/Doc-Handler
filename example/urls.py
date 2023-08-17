df88d03a739caa744758e63f80868999cd07ef90
Swatantra Singh <78965726+transfinite-rathore@users.noreply.github.com>
2023-08-17 15:32:37 +0530

    modified:   example/admin.py 	new file:   example/models.py 	modified:   example/urls.py 	modified:   example/views.py 	modified:   requirements.txt
    Changes not staged for commit:
    	modified:   example/admin.py
    	modified:   example/urls.py
    	modified:   example/views.py
    	modified:   vercel_app/urls.py

--- example/urls.py
+++ example/urls.py
@@ -9,7 +9,7 @@
 # ]
 from django.contrib import admin
 from django.urls import path
-from account.views import *
+from example.views import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
--- example/views.py
+++ example/views.py
@@ -5,6 +5,7 @@
 # from django.shortcuts import render
 # def index(request):
 #     return render(request,"index.html")
+
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
