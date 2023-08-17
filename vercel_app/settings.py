90f01cb21e8d2a1cd21bd02ac60d78fef2f21694
Swatantra Singh <78965726+transfinite-rathore@users.noreply.github.com>
2023-08-17 16:42:27 +0530

    deleted:    public/static/documnet/peakpx_5.jpg 	modified:   vercel_app/settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -118,18 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR , "hotels/static")
-}
-
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
